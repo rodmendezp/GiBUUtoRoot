d4d6b0cd9524aad0ef5621b67e225ad2c1465551
Rodrigo Mendez <rodmendezp@gmail.com>
2016-04-07 13:12:47 -0300

    Modified particlesToRoot, still need work to do

--- Particles/particlesToRoot.cpp
+++ Particles/particlesToRoot.cpp
@@ -14,7 +14,7 @@ TString rootfName;
 
 int main(int argc,  char **argv){
 	// Arg 1 is the filename
-	rootfName = "part.root"
+	rootfName = "part.root";
 	if(argc > 1)
 		fName = (TString) argv[1];
 	if(argc > 2)
@@ -36,11 +36,34 @@ int main(int argc,  char **argv){
 		std::cout << "Error opening " << fName << std::endl;
 		return -1;	
 	}
+	
+	Int_t nParts;
+	Int_t partID;
+	Int_t eventType;
+	Float_t weight;
+	Float_t unused;
+	Float_t px, py, pz;
+	Float_t e;
+	Float_t m;
+	Float_t spin;
+	Float_t nu;
+	Float_t q2;
+	Float_t eps;
+	Float_t phiL;
 			
 	while(!in.eof()){
 		in >> tLine;
-		if(tLine == "<event>")		
-			std::cout << tLine << std::endl;
+		if(tLine == "<event>"){
+			in >> nParts >> unused >> weight >> unused >> unused >> unused;
+			for(Int_t i = 0; i < nParts; i++){
+				in >> partID >> unused >> unused >> unused >> unused >> unused >> px >> py >> pz >> e >> m >> unused >> spin;
+				// Calculate all the missing variables
+				//std::cout << partID << "\t" << px << "\t" << py << "\t" << pz << "\t" << e << "\t" << m << "\t" << spin << std::endl;
+			}
+			in >> tLine >> unused >> nu >> q2 >> eps >> phiL >> eventType;
+			//std::cout << tLine << "\t" << nu << "\t" << q2 << "\t" << eps << "\t" << phiL << "\t" << eventType << std::endl;
+			//ntuple_elec->Fill(1., q2, nu,)
+		}
 	}
 	
 	f->cd();
